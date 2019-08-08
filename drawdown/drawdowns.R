d21c69abf24ff5f2525af2a3825833edf8e81eac
Shyam Sunder <shyam.udupa@gmail.com>
2019-08-08 22:10:01 +0530

    changed pandoc path

--- drawdown/drawdowns.R
+++ drawdown/drawdowns.R
@@ -1,3 +1,3 @@
 library(rmarkdown)
-Sys.setenv(RSTUDIO_PANDOC="C:\Program Files\Pandoc\")
+Sys.setenv(RSTUDIO_PANDOC="C:/Program Files/Pandoc")
 render("drawdowns.Rmd", output_file="drawdowns.html")
