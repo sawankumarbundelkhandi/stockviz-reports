d7b0eb0a4bccea66c5835bcad7883bcdb72edc13
Shyam Sunder <shyam.udupa@gmail.com>
2020-04-28 10:54:07 +0530

    52-week highs and lows for Indian and US indices

---
+++ 52-week-highs-lows/52wk-IN.R
@@ -0,0 +1,3 @@
+library(rmarkdown)
+Sys.setenv(RSTUDIO_PANDOC="C:/Program Files/Pandoc")
+render("52wk-IN.Rmd", output_file="52wk-IN.html")
---
+++ 52-week-highs-lows/52wk-US.R
@@ -0,0 +1,3 @@
+library(rmarkdown)
+Sys.setenv(RSTUDIO_PANDOC="C:/Program Files/Pandoc")
+render("52wk-US.Rmd", output_file="52wk-US.html")
