1aa6e18017edd21eb09de713f468e5b4d422e77f
Shyam Sunder <shyam.udupa@gmail.com>
2021-09-01 13:54:22 +0530

    refresh

---
+++ all-time-highs/ath-IN.R
@@ -0,0 +1,3 @@
+library(rmarkdown)
+Sys.setenv(RSTUDIO_PANDOC="C:/Program Files/Pandoc")
+render("ath-IN.Rmd", output_file="ath-IN.html")
---
+++ all-time-highs/ath-US.R
@@ -0,0 +1,3 @@
+library(rmarkdown)
+Sys.setenv(RSTUDIO_PANDOC="C:/Program Files/Pandoc")
+render("ath-US.Rmd", output_file="ath-US.html")
