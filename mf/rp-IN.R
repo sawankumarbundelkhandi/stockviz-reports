fb514a33e4790359b7cff87a255c3c646b224c61
Shyam Sunder <shyam.udupa@gmail.com>
2022-12-23 11:24:28 +0530

    added mutual fund tear-sheets

--- mf/rp-IN.R
+++ mf/rp-IN.R
@@ -1,3 +1,55 @@
-library(rmarkdown)
+library('RODBC')
+library('rmarkdown')
+
 Sys.setenv(RSTUDIO_PANDOC="C:/Program Files/Pandoc")
-render("rp-IN.Rmd", output_file="rp-IN.html")
+
+source("C:/stockviz/r/config.r")
+
+lcon <- odbcDriverConnect(sprintf("Driver={ODBC Driver 17 for SQL Server};Server=%s;Database=%s;Uid=%s;Pwd=%s;", ldbserver, ldbname, ldbuser, ldbpassword), case = "nochange", believeNRows = TRUE)
+
+asofDt<-sqlQuery(lcon, "select max(as_of) from mf_nav_history")[[1]]
+startDate <- Sys.Date() - 5*365
+
+print("subsetting mutual funds...")
+
+largecapFunds <- sqlQuery(lcon, sprintf("select scheme_code, min(as_of) stdt, max(as_of) ltdt from mf_nav_history where 
+										scheme_name like '%%large%%direct%%' 
+										and scheme_name like '%%growth%%' 
+										and scheme_name not like '%%idcw%%' 
+										and scheme_name not like '%%bonus%%' 
+										and scheme_name not like '%%mid%%' 
+										and scheme_name not like '%%small%%' 
+										and scheme_name not like '%%tax%%' 
+										and as_of >= '%s' 
+										group by scheme_code", startDate))
+largecapFunds <- largecapFunds[largecapFunds$stdt <= startDate+180 & largecapFunds$ltdt >= asofDt-5,]
+
+midcapFunds <- sqlQuery(lcon, sprintf("select scheme_code, min(as_of) stdt, max(as_of) ltdt from mf_nav_history where 
+										scheme_name like '%%mid%%cap%%direct%%' 
+										and scheme_name like '%%growth%%' 
+										and scheme_name not like '%%idcw%%' 
+										and scheme_name not like '%%bonus%%' 
+										and scheme_name not like '%%large%%' 
+										and scheme_name not like '%%small%%' 
+										and scheme_name not like '%%tax%%' 
+										and as_of >= '%s' 
+										group by scheme_code", startDate))
+midcapFunds <- midcapFunds[midcapFunds$stdt <= startDate+180 & midcapFunds$ltdt >= asofDt-5,]
+
+print("rendering master page...")
+
+render("rp-IN.Rmd", output_file="rp-IN.html", params=list(LARGECAP_SCHEME_CODES = largecapFunds$scheme_code, MIDCAP_SCHEME_CODES = midcapFunds$scheme_code, ST_DT = startDate, ASOF_DT = asofDt))
+
+#q()
+
+print("rendering large caps...")
+for(sc in largecapFunds$scheme_code){
+	print(paste(sc, 'LARGECAP'))
+	render("rp-mf.Rmd", output_file=paste0("analysis/rp-", sc, ".html"), params=list(scheme_code = sc, benchmark = 'LARGECAP'))
+}
+
+print("rendering midcaps caps...")
+for(sc in midcapFunds$scheme_code){
+	print(paste(sc, 'MIDCAP'))
+	render("rp-mf.Rmd", output_file=paste0("analysis/rp-", sc, ".html"), params=list(scheme_code = sc, benchmark = 'MIDCAP'))
+}
