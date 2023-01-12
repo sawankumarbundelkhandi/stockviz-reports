4cddea1a2bd7d69dabfd4b690c4e368b35fa8779
Shyam Sunder <shyam.udupa@gmail.com>
2023-01-12 13:38:08 +0530

    manual macro refresh

--- macro/macro.R
+++ macro/macro.R
@@ -1,6 +1,6 @@
 library(rmarkdown)
 Sys.setenv(RSTUDIO_PANDOC="C:/Program Files/Pandoc")
-libPath <- "C:/StockViz/public/reports/rmdlibs"
-render("commodities/rp-cmdy.Rmd", output_file="rp-cmdy.html", output_options=list(html_document = list(self_contained = FALSE, lib_dir = libPath)))
-render("bonds/rp-bnd.Rmd", output_file="rp-bnd.html", output_options=list(html_document = list(self_contained = FALSE, lib_dir = libPath)))
-render("currencies/rp-cur.Rmd", output_file="rp-cur.html", output_options=list(html_document = list(self_contained = FALSE, lib_dir = libPath)))
+
+render("rp-cmdy.Rmd", output_file="rp-cmdy.html")
+render("rp-bnd.Rmd", output_file="rp-bnd.html")
+render("rp-cur.Rmd", output_file="rp-cur.html")
