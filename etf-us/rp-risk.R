3f2439a3422e8e76fe0bc503d990bebe3c6422b4
Shyam Sunder <shyam.udupa@gmail.com>
2023-01-15 09:12:47 +0530

    manual refresh

--- etf-us/rp-risk.R
+++ etf-us/rp-risk.R
@@ -10,12 +10,13 @@ options(stringsAsFactors = FALSE)
 
 lconUs2 <- odbcDriverConnect(sprintf("Driver={ODBC Driver 17 for SQL Server};Server=%s;Database=%s;Uid=%s;Pwd=%s;", ldbserver, "StockVizUs2", ldbuser, ldbpassword), case = "nochange", believeNRows = TRUE)
 
-#render("rp-country.Rmd", output_file="rp-country.html")
-#render("rp-risk.Rmd", output_file="rp-risk.html")
-#q()
+render("rp-country.Rmd", output_file="rp-country.html")
+render("rp-risk.Rmd", output_file="rp-risk.html")
+q()
+
+maxDt <- sqlQuery(lconUs2, "select max(time_stamp) from EQ_CAPM") [[1]]
 
 renderEtfs <- function(){
-	maxDt <- sqlQuery(lconUs2, "select max(time_stamp) from EQ_CAPM") [[1]]
 	etfTickers <- sqlQuery(lconUs2, sprintf("select distinct symbol from eq_capm where time_stamp = '%s'", maxDt))[,1]
 
 	for(i in 1:length(etfTickers)){
